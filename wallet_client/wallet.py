d1b09f3ed629359cbd1324b2d13e65d266a4a344
OKrass <37540459+OKrass@users.noreply.github.com>
2022-10-17 23:54:03 +0200

    added encryption of private key (#2)
    * added encryption of private key
    
    * small tweaks to make it work with sending messages
    
    * removed print
    
    Co-authored-by: Krasnyi <krasnyio@emea.roche.com>
    Co-authored-by: TommyV2 <coconutv123@gmail.com>

--- wallet_client/wallet.py
+++ wallet_client/wallet.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 import ecdsa
+from cryptography.fernet import Fernet
 
 # import requests
 
@@ -30,6 +31,7 @@ class Wallet:
 
     # To generate public and private key using ECDSA with SECP256k1 curve
     def generate_keys(self):
+        encryption_key = Fernet.generate_key()
         signing_key = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)  # signing key
         private_key = signing_key.to_string().hex()  # private key in hex
         verification_key = signing_key.get_verifying_key()  # verification key
@@ -39,24 +41,50 @@ class Wallet:
         storage_path = "wallet_client/secrets_storage"
         self.create_folder(storage_path)
         self.create_folder(f"{storage_path}/secret_{self.NODE_PORT}")
-        with open(f"{storage_path}/secret_{self.NODE_PORT}/pub_key", "w") as f:
+        path = f"{storage_path}/secret_{self.NODE_PORT}"
+        self.key_write(encryption_key, f"{path}/encryption_key")
+        with open(f"{path}/pub_key", "w") as f:
             f.write(f"{public_key.decode()}")
-        with open(f"{storage_path}/secret_{self.NODE_PORT}/priv_key", "w") as f:
+        with open(f"{path}/priv_key", "w") as f:
             f.write(f"{private_key}")
+        self.file_encrypt(encryption_key, f"{path}/priv_key", f"{path}/enc_priv_key")
+        os.remove(f"{path}/priv_key")
 
         print("=========================================")
         print('New keys generated in the "secret" folder')
+        print("This is your private key:")
+        print("=========================================")
+        print(private_key)
         print("=========================================")
 
-    # Get public key for Node with chosen port
-    def get_pub_key(self, port):
-        storage_path = "wallet_client/secrets_storage"
-        with open(f"{storage_path}/secret_{port}/pub_key", "r") as f:
-            pub_key = f.read()
-
-        return pub_key
-
     # Helper Wallet methods
+
+    def key_write(self, key, key_name):
+        with open(key_name, "wb") as mykey:
+            mykey.write(key)
+
+    def key_load(self, port, key_name):
+        with open(f"wallet_client/secrets_storage/secret_{port}/{key_name}", "r") as mykey:
+            key = mykey.read()
+        return key
+
+    def file_encrypt(self, key, original_file, encrypted_file):
+        f = Fernet(key)
+        with open(original_file, "rb") as file:
+            original = file.read()
+
+        encrypted = f.encrypt(original)
+        with open(encrypted_file, "wb") as file:
+            file.write(encrypted)
+          
+    def file_decrypt(self, key, encrypted_file, decrypted_file):
+        f = Fernet(key)
+        with open(encrypted_file, "rb") as file:
+            encrypted = file.read()
+
+        decrypted = f.decrypt(encrypted)
+        with open(decrypted_file, "wb") as file:
+            file.write(decrypted)
 
     def create_folder(self, path):
         exists = os.path.exists(path)
